7d43aa462788e1d9f3f4833ff27da20dcb79cb0c
YechanJung <127584126+YechanJung@users.noreply.github.com>
2024-11-16 20:41:00 +0900

    update

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -1,17 +1,39 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
-
+import FullScreenMessage from './components/shared/FullScreenMessage'
 const cx = classNames.bind(styles)
 
 function App() {
-  useEffect(() => {
-    fetch('http://localhost:8888/posts')
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-  }, [])
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  return <div className={cx('container')}></div>
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8888asdf/wedding')
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+
+        throw new Error('Failed to fetch wedding data')
+      })
+      .then((data) => {
+        setWedding(data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        setError(error)
+        setLoading(false)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+  if (loading) return <FullScreenMessage type="loading" />
+  if (error) return <FullScreenMessage type="error" />
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
